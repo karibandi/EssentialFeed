09db4f203dbdfa24d7f5d4a0704454ed0e23187a
Surendra Karibandi <surendrakaribandi@gmail.com>
2021-07-04 19:58:15 +0530

    Initial commit

---
+++ EssentialFeed/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by Surendra Karibandi on 04/07/21.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}
