387f08503e0c83f0b6b2f50d45dfa19e0ca1fff7
Surendra Karibandi <surendrakaribandi@gmail.com>
2021-07-06 13:31:02 +0530

    Remove  hard coded url from  feed loader and make it generic

--- EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -9,13 +9,15 @@ import XCTest
 
 class RemoteFeedLoader{
     let client: HTTPClient
+    let url: URL
     
-    init(client: HTTPClient) {
+    init(url: URL, client: HTTPClient) {
         self.client = client
+        self.url = url
     }
     
     func load()  {
-        client.get(from: URL(string: "https://www.google.com")!)
+        client.get(from: url)
     }
 }
 
@@ -35,14 +37,17 @@ class HTTPClientSpy: HTTPClient {
 class RemoteFeedLoaderTests: XCTestCase {
     func test_Init_doesNotRequestDataFromURL() {
         let client = HTTPClientSpy()
-        let _ = RemoteFeedLoader(client: client)
+        let url = URL(string: "www.goole.com")!
+        
+        let _ = RemoteFeedLoader(url: url, client: client)
         XCTAssertNil(client.requestedURL)
     }
     
     func test_init_requestDataFromURL(){
         let client = HTTPClientSpy()
-        let sut = RemoteFeedLoader(client: client)
+        let url = URL(string: "www.google.com")!
+        let sut = RemoteFeedLoader(url: url, client: client)
         sut.load()
-        XCTAssertNotNil(client.requestedURL)
+        XCTAssertEqual(client.requestedURL, url)
     }
 }
